f844013fddf28734dfcaddd64091741eff7e2379
Jan Funke <funke@ini.ch>
2012-06-26 19:39:08 -0400

    implemented texture rendering for SegmentsPainter
    …this saves a lot of polygons to be rendered.

--- sopnet/gui/SegmentsPainter.cpp
+++ sopnet/gui/SegmentsPainter.cpp
@@ -1,19 +1,29 @@
-#include "SegmentsPainter.h"
+#include <boost/array.hpp>
+
+#include <gui/OpenGl.h>
 #include <imageprocessing/ConnectedComponent.h>
 #include <sopnet/EndSegment.h>
 #include <sopnet/ContinuationSegment.h>
 #include <sopnet/BranchSegment.h>
 #include <util/foreach.h>
+#include "SegmentsPainter.h"
 
 logger::LogChannel segmentspainterlog("segmentspainterlog", "[SegmentsPainter] ");
 
 SegmentsPainter::SegmentsPainter() :
 	_zScale(8) {}
 
+SegmentsPainter::~SegmentsPainter() {
+
+	deleteTextures();
+}
+
 void
 SegmentsPainter::setSegments(boost::shared_ptr<Segments> segments) {
 
 	_segments = segments;
+
+	loadTextures();
 
 	util::rect<double> size(0, 0, 0, 0);
 
@@ -25,6 +35,87 @@ SegmentsPainter::setSegments(boost::shared_ptr<Segments> segments) {
 }
 
 void
+SegmentsPainter::loadTextures() {
+
+	LOG_DEBUG(segmentspainterlog) << "loading textures..." << std::endl;
+
+	deleteTextures();
+
+	foreach (boost::shared_ptr<EndSegment> segment, _segments->getEnds())
+		loadTextures(*segment);
+
+	foreach (boost::shared_ptr<ContinuationSegment> segment, _segments->getContinuations())
+		loadTextures(*segment);
+
+	foreach (boost::shared_ptr<BranchSegment> segment, _segments->getBranches())
+		loadTextures(*segment);
+
+	LOG_DEBUG(segmentspainterlog) << "all textures loaded..." << std::endl;
+}
+
+void
+SegmentsPainter::loadTextures(const EndSegment& end) {
+
+	loadTexture(*end.getSlice());
+}
+
+void
+SegmentsPainter::loadTextures(const ContinuationSegment& continuation) {
+
+	loadTexture(*continuation.getSourceSlice());
+	loadTexture(*continuation.getTargetSlice());
+}
+
+void
+SegmentsPainter::loadTextures(const BranchSegment& branch) {
+
+	loadTexture(*branch.getSourceSlice());
+	loadTexture(*branch.getTargetSlice1());
+	loadTexture(*branch.getTargetSlice2());
+}
+
+void
+SegmentsPainter::loadTexture(const Slice& slice) {
+
+	if (_textures.count(slice.getId()))
+		return;
+
+	// create image data
+	const util::rect<double> bb = slice.getComponent()->getBoundingBox();
+
+	util::point<unsigned int> size(bb.maxX - bb.minX + 2, bb.maxY - bb.minY + 2);
+	util::point<unsigned int> offset(bb.minX, bb.minY);
+
+	// rgba data
+	std::vector<boost::array<float, 4> > pixels;
+
+	// fill with opaque value
+
+	boost::array<float, 4> opaque;
+	opaque[0] = 0.0;
+	opaque[1] = 0.0;
+	opaque[2] = 0.0;
+	opaque[3] = 0.0;
+	pixels.resize(size.x*size.y, opaque);
+
+	foreach (const util::point<unsigned int>& pixel, slice.getComponent()->getPixels()) {
+
+		unsigned int index = (pixel.x - offset.x) + (pixel.y - offset.y)*size.x;
+
+		pixels[index][0] = 1.0;
+		pixels[index][1] = 1.0;
+		pixels[index][2] = 1.0;
+		pixels[index][3] = 1.0;
+	}
+
+	gui::Texture* texture = new gui::Texture(size.x, size.y, GL_RGBA);
+
+	texture->loadData(pixels.begin());
+
+	_textures[slice.getId()] = texture;
+}
+
+void
 SegmentsPainter::updateRecording() {
 
 	// make sure OpenGl operations are save
@@ -33,42 +124,58 @@ SegmentsPainter::updateRecording() {
 	startRecording();
 
 	GLfloat ambient[4] = { 0, 0, 0, 1 };
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+	glCheck(glLightfv(GL_LIGHT0, GL_AMBIENT, ambient));
 	GLfloat diffuse[4] = { 0.1, 0.1, 0.1, 1 };
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	glCheck(glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse));
 	GLfloat specular[4] = { 0.1, 0.1, 0.1, 1 };
-	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+	glCheck(glLightfv(GL_LIGHT0, GL_SPECULAR, specular));
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+	glCheck(glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular));
 	GLfloat emission[4] = { 0, 0, 0, 1 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, emission);
+	glCheck(glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, emission));
 
 	// enable alpha blending
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glCheck(glEnable(GL_BLEND));
+	glCheck(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-	// always draw the components
-	glDisable(GL_CULL_FACE);
+	glCheck(glEnable(GL_CULL_FACE));
+	glCheck(glEnable(GL_LIGHTING));
+	glCheck(glEnable(GL_LIGHT0));
+	glCheck(glEnable(GL_COLOR_MATERIAL));
 
-	glEnable(GL_DEPTH_TEST);
+	// draw from front to back, facing to the right
+	_leftSide  = false;
+	_rightSide = true;
+	for (int i = 0; i < _segments->getNumInterSectionIntervals(); i++) {
 
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_COLOR_MATERIAL);
-	glDisable(GL_TEXTURE_2D);
+		foreach (boost::shared_ptr<EndSegment> segment, _segments->getEnds(i))
+			draw(*segment);
 
-	foreach (boost::shared_ptr<EndSegment> segment, _segments->getEnds())
-		draw(*segment);
+		foreach (boost::shared_ptr<ContinuationSegment> segment, _segments->getContinuations(i))
+			draw(*segment);
 
-	foreach (boost::shared_ptr<ContinuationSegment> segment, _segments->getContinuations())
-		draw(*segment);
+		foreach (boost::shared_ptr<BranchSegment> segment, _segments->getBranches(i))
+			draw(*segment);
+	}
 
-	foreach (boost::shared_ptr<BranchSegment> segment, _segments->getBranches())
-		draw(*segment);
+	// draw from back to front, facing to the left
+	_leftSide  = true;
+	_rightSide = false;
+	for (int i = _segments->getNumInterSectionIntervals() - 1; i >= 0; i--) {
 
-	glDisable(GL_BLEND);
+		foreach (boost::shared_ptr<EndSegment> segment, _segments->getEnds(i))
+			draw(*segment);
 
-	glDisable(GL_LIGHTING);
+		foreach (boost::shared_ptr<ContinuationSegment> segment, _segments->getContinuations(i))
+			draw(*segment);
+
+		foreach (boost::shared_ptr<BranchSegment> segment, _segments->getBranches(i))
+			draw(*segment);
+	}
+
+	glCheck(glDisable(GL_BLEND));
+	glCheck(glDisable(GL_LIGHTING));
+	glCheck(glDisable(GL_CULL_FACE));
 
 	stopRecording();
 }
@@ -76,29 +183,20 @@ SegmentsPainter::updateRecording() {
 void
 SegmentsPainter::draw(const EndSegment& end) {
 
-	drawSlice(
-			end.getSlice(),
-			1.0, 0.0, 0.0,
-			(end.getDirection() == Left  ? true : false),
-			(end.getDirection() == Right ? true : false),
-			true);
+	glCheck(glEnable(GL_TEXTURE_2D));
+
+	drawSlice(end.getSlice(), 1.0, 0.0, 0.0);
+
+	glCheck(glDisable(GL_TEXTURE_2D));
 }
 
 void
 SegmentsPainter::draw(const ContinuationSegment& continuation) {
 
-	drawSlice(
-			continuation.getSourceSlice(),
-			0.0, 1.0, 0.0,
-			(continuation.getDirection() == Left ? true  : false),
-			(continuation.getDirection() == Left ? false : true),
-			true);
-	drawSlice(
-			continuation.getTargetSlice(),
-			0.0, 1.0, 0.0,
-			(continuation.getDirection() == Left ? false : true),
-			(continuation.getDirection() == Left ? true  : false),
-			true);
+	glCheck(glEnable(GL_TEXTURE_2D));
+
+	drawSlice(continuation.getSourceSlice(), 0.0, 1.0, 0.0);
+	drawSlice(continuation.getTargetSlice(), 0.0, 1.0, 0.0);
 
 	util::point<double> center1 = continuation.getSourceSlice()->getComponent()->getCenter();
 	util::point<double> center2 = continuation.getTargetSlice()->getComponent()->getCenter();
@@ -106,35 +204,24 @@ SegmentsPainter::draw(const ContinuationSegment& continuation) {
 	unsigned int section1 = continuation.getSourceSlice()->getSection();
 	unsigned int section2 = continuation.getTargetSlice()->getSection();
 
-	glBegin(GL_LINES);
+	glCheck(glDisable(GL_TEXTURE_2D));
 
-	glVertex3f(center1.x, center1.y, section1*_zScale);
-	glVertex3f(center2.x, center2.y, section2*_zScale);
+	glCheck(glBegin(GL_LINES));
 
-	glEnd();
+	glCheck(glVertex3f(center1.x, center1.y, section1*_zScale));
+	glCheck(glVertex3f(center2.x, center2.y, section2*_zScale));
+
+	glCheck(glEnd());
 }
 
 void
 SegmentsPainter::draw(const BranchSegment& branch) {
 
-	drawSlice(
-			branch.getSourceSlice(),
-			0.0, 0.0, 1.0,
-			(branch.getDirection() == Left ? true  : false),
-			(branch.getDirection() == Left ? false : true),
-			true);
-	drawSlice(
-			branch.getTargetSlice1(),
-			0.0, 0.0, 1.0,
-			(branch.getDirection() == Left ? false : true),
-			(branch.getDirection() == Left ? true  : false),
-			true);
-	drawSlice(
-			branch.getTargetSlice2(),
-			0.0, 0.0, 1.0,
-			(branch.getDirection() == Left ? false : true),
-			(branch.getDirection() == Left ? true  : false),
-			true);
+	glCheck(glEnable(GL_TEXTURE_2D));
+
+	drawSlice(branch.getSourceSlice(), 0.0, 0.0, 1.0);
+	drawSlice(branch.getTargetSlice1(), 0.0, 0.0, 1.0);
+	drawSlice(branch.getTargetSlice2(), 0.0, 0.0, 1.0);
 
 	util::point<double> center1 = branch.getSourceSlice()->getComponent()->getCenter();
 	util::point<double> center2 = branch.getTargetSlice1()->getComponent()->getCenter();
@@ -144,134 +231,85 @@ SegmentsPainter::draw(const BranchSegment& branch) {
 	unsigned int section2 = branch.getTargetSlice1()->getSection();
 	unsigned int section3 = branch.getTargetSlice2()->getSection();
 
-	glBegin(GL_LINES);
+	glCheck(glDisable(GL_TEXTURE_2D));
 
-	glVertex3f(center1.x, center1.y, section1*_zScale);
-	glVertex3f(center2.x, center2.y, section2*_zScale);
+	glCheck(glBegin(GL_LINES));
 
-	glEnd();
+	glCheck(glVertex3f(center1.x, center1.y, section1*_zScale));
+	glCheck(glVertex3f(center2.x, center2.y, section2*_zScale));
 
-	glBegin(GL_LINES);
+	glCheck(glEnd());
 
-	glVertex3f(center1.x, center1.y, section1*_zScale);
-	glVertex3f(center3.x, center3.y, section3*_zScale);
+	glCheck(glBegin(GL_LINES));
 
-	glEnd();
+	glCheck(glVertex3f(center1.x, center1.y, section1*_zScale));
+	glCheck(glVertex3f(center3.x, center3.y, section3*_zScale));
+
+	glCheck(glEnd());
 }
 
 void
 SegmentsPainter::drawSlice(
 		boost::shared_ptr<Slice> slice,
-		double red, double green, double blue,
-		bool leftSide, bool rightSide,
-		bool surface) {
-
-	boost::shared_ptr<ConnectedComponent> component = slice->getComponent();
+		double red, double green, double blue) {
 
 	double section = slice->getSection();
 
-	double minZ = _zScale*(section - (leftSide  ? 0.5 : 0));
-	double maxZ = _zScale*(section + (rightSide ? 0.5 : 0));
+	LOG_ALL(segmentspainterlog)
+			<< "drawing slice " << slice->getId()
+			<< " in section " << section << std::endl;
 
-	unsigned int width   = (unsigned int)(component->getBoundingBox().width()  + 1);
-	unsigned int height  = (unsigned int)(component->getBoundingBox().height() + 1);
-	unsigned int offsetX = (unsigned int)component->getBoundingBox().minX;
-	unsigned int offsetY = (unsigned int)component->getBoundingBox().minY;
+	double z = _zScale*section;
 
-	std::vector<bool> pixels(width*height, false);
+	glCheck(glColor4f(red, green, blue, 1.0));
 
-	unsigned int numPixels = 0;
-	foreach (const util::point<unsigned int>& p, component->getPixels()) {
+	_textures[slice->getId()]->bind();
 
-		unsigned int x = p.x - offsetX;
-		unsigned int y = p.y - offsetY;
+	glCheck(glBegin(GL_QUADS));
 
-		if (x + y*width < width*height) {
+	const util::rect<double>& bb = slice->getComponent()->getBoundingBox();
 
-			pixels[x + y*width] = true;
-			numPixels++;
+	if (_leftSide) {
 
-		} else {
-
-			LOG_ERROR(segmentspainterlog) << "you are trying to access a pixel at " << p << " that is not within " << component->getBoundingBox() << std::endl;
-		}
+		glTexCoord2d(1.0, 0.0); glNormal3d(0, 0, -1); glVertex3d(bb.maxX, bb.minY, z);
+		glTexCoord2d(1.0, 1.0); glNormal3d(0, 0, -1); glVertex3d(bb.maxX, bb.maxY, z);
+		glTexCoord2d(0.0, 1.0); glNormal3d(0, 0, -1); glVertex3d(bb.minX, bb.maxY, z);
+		glTexCoord2d(0.0, 0.0); glNormal3d(0, 0, -1); glVertex3d(bb.minX, bb.minY, z);
 	}
 
-	LOG_ALL(segmentspainterlog) << "drawing a slice with " << numPixels << " pixels..." << std::endl;
+	if (_rightSide) {
 
-	glColor4f(red, green, blue, 1.0);
-
-	glBegin(GL_QUADS);
-
-	foreach (const util::point<unsigned int>& p, component->getPixels()) {
-
-		// draw left or right side
-		if (leftSide) {
-
-			glNormal3d(0, 0, -1); glVertex3d(p.x,     p.y,     minZ);
-			glNormal3d(0, 0, -1); glVertex3d(p.x + 1, p.y,     minZ);
-			glNormal3d(0, 0, -1); glVertex3d(p.x + 1, p.y + 1, minZ);
-			glNormal3d(0, 0, -1); glVertex3d(p.x,     p.y + 1, minZ);
-		}
-
-		if (rightSide) {
-
-			glNormal3d(0, 0, 1);  glVertex3d(p.x,     p.y,     maxZ);
-			glNormal3d(0, 0, 1);  glVertex3d(p.x + 1, p.y,     maxZ);
-			glNormal3d(0, 0, 1);  glVertex3d(p.x + 1, p.y + 1, maxZ);
-			glNormal3d(0, 0, 1);  glVertex3d(p.x,     p.y + 1, maxZ);
-		}
-
-		// only draw surface of visible pixels
-
-		// for neighborhood testing with offset
-		unsigned int x = p.x - offsetX;
-		unsigned int y = p.y - offsetY;
-
-		if (x > 0 && x < width - 1 && y > 0 && y < height - 1 &&
-		    pixels[(x - 1) + (y - 1)*width] &&
-		    pixels[(x - 1) + (y + 1)*width] &&
-		    pixels[(x + 1) + (y + 1)*width] &&
-		    pixels[(x + 1) + (y - 1)*width])
-			continue;
-
-		if (surface) {
-
-			glNormal3d(0, -1, 0); glVertex3d(p.x,     p.y,     minZ);
-			glNormal3d(0, -1, 0); glVertex3d(p.x + 1, p.y,     minZ);
-			glNormal3d(0, -1, 0); glVertex3d(p.x + 1, p.y,     maxZ);
-			glNormal3d(0, -1, 0); glVertex3d(p.x,     p.y,     maxZ);
-
-			glNormal3d(-1, 0, 0); glVertex3d(p.x,     p.y,     minZ);
-			glNormal3d(-1, 0, 0); glVertex3d(p.x,     p.y + 1, minZ);
-			glNormal3d(-1, 0, 0); glVertex3d(p.x,     p.y + 1, maxZ);
-			glNormal3d(-1, 0, 0); glVertex3d(p.x,     p.y,     maxZ);
-
-			glNormal3d(0, 1, 0); glVertex3d(p.x + 1, p.y + 1, minZ);
-			glNormal3d(0, 1, 0); glVertex3d(p.x,     p.y + 1, minZ);
-			glNormal3d(0, 1, 0); glVertex3d(p.x,     p.y + 1, maxZ);
-			glNormal3d(0, 1, 0); glVertex3d(p.x + 1, p.y + 1, maxZ);
-
-			glNormal3d(1, 0, 0); glVertex3d(p.x + 1, p.y + 1, minZ);
-			glNormal3d(1, 0, 0); glVertex3d(p.x + 1, p.y,     minZ);
-			glNormal3d(1, 0, 0); glVertex3d(p.x + 1, p.y,     maxZ);
-			glNormal3d(1, 0, 0); glVertex3d(p.x + 1, p.y + 1, maxZ);
-		}
+		glTexCoord2d(0.0, 0.0); glNormal3d(0, 0, 1); glVertex3d(bb.minX, bb.minY, z);
+		glTexCoord2d(0.0, 1.0); glNormal3d(0, 0, 1); glVertex3d(bb.minX, bb.maxY, z);
+		glTexCoord2d(1.0, 1.0); glNormal3d(0, 0, 1); glVertex3d(bb.maxX, bb.maxY, z);
+		glTexCoord2d(1.0, 0.0); glNormal3d(0, 0, 1); glVertex3d(bb.maxX, bb.minY, z);
 	}
 
-	glEnd();
+	glCheck(glEnd());
 
 	LOG_ALL(segmentspainterlog) << "done." << std::endl;
 
 	if (_size == util::rect<double>(0, 0, 0, 0))
 
-		_size = component->getBoundingBox();
+		_size = bb;
 
 	else {
 
-		_size.minX = std::min(_size.minX, component->getBoundingBox().minX);
-		_size.minY = std::min(_size.minY, component->getBoundingBox().minY);
-		_size.maxX = std::max(_size.maxX, component->getBoundingBox().maxX);
-		_size.maxY = std::max(_size.maxY, component->getBoundingBox().maxY);
+		_size.minX = std::min(_size.minX, bb.minX);
+		_size.minY = std::min(_size.minY, bb.minY);
+		_size.maxX = std::max(_size.maxX, bb.maxX);
+		_size.maxY = std::max(_size.maxY, bb.maxY);
 	}
+}
+
+void
+SegmentsPainter::deleteTextures() {
+
+	unsigned int id;
+	gui::Texture* texture;
+	foreach (boost::tie(id, texture), _textures)
+		if (texture)
+			delete texture;
+
+	_textures.clear();
 }
--- sopnet/gui/SegmentsPainter.h
+++ sopnet/gui/SegmentsPainter.h
@@ -2,6 +2,7 @@
 #define CELLTRACKER_GUI_TRACKLETS_PAINTER_H__
 
 #include <gui/RecordablePainter.h>
+#include <gui/Texture.h>
 #include <sopnet/Segments.h>
 
 class SegmentsPainter : public gui::RecordablePainter {
@@ -10,9 +11,21 @@ public:
 
 	SegmentsPainter();
 
+	~SegmentsPainter();
+
 	void setSegments(boost::shared_ptr<Segments> segments);
 
 private:
+
+	void loadTextures();
+
+	void loadTextures(const EndSegment& end);
+
+	void loadTextures(const ContinuationSegment& continuation);
+
+	void loadTextures(const BranchSegment& branch);
+
+	void loadTexture(const Slice& slice);
 
 	void updateRecording();
 
@@ -22,17 +35,23 @@ private:
 
 	void draw(const BranchSegment& branch);
 
-	void drawSlice(
-			boost::shared_ptr<Slice> slice,
-			double red, double green, double blue,
-			bool leftSide, bool rightSide,
-			bool surface);
+	void drawSlice(boost::shared_ptr<Slice> slice, double red, double green, double blue);
+
+	void deleteTextures();
 
 	boost::shared_ptr<Segments> _segments;
 
 	util::rect<double> _size;
 
+	// the distance between two sections
 	double _zScale;
+
+	// slice textures from slice ids
+	std::map<unsigned int, gui::Texture*> _textures;
+
+	// which side of the faces to draw
+	bool _leftSide;
+	bool _rightSide;
 };
 
 #endif // CELLTRACKER_GUI_TRACKLETS_PAINTER_H__
