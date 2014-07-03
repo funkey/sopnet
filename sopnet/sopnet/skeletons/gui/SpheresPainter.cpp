ed373157fc784f4815976e1cba44fb1b418ffb68
Jan Funke <funke@ini.ch>
2014-07-03 19:59:25 +0200

    implemented SpheresPainter

--- sopnet/sopnet/skeletons/gui/SpheresPainter.cpp
+++ sopnet/sopnet/skeletons/gui/SpheresPainter.cpp
@@ -1,3 +1,5 @@
+#include <util/foreach.h>
+#include <gui/OpenGl.h>
 #include "SpheresPainter.h"
 
 void
@@ -22,7 +24,39 @@ SpheresPainter::updateRecording() {
 
 	startRecording();
 
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHT0);
+	glDisable(GL_CULL_FACE);
+	glColor3f(1.0, 0.2, 0.4);
+	GLfloat lightpos[] = {0.5, 1.0, 1.0, 0.0};
+	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
 
+	GLUquadric* quadric = gluNewQuadric();
+
+	if (quadric) {
+
+		foreach (const Sphere& sphere, *_spheres) {
+
+			glPushMatrix();
+			glTranslated(sphere.getX(), sphere.getY(), sphere.getZ());
+			gluSphere(quadric, sphere.getRadius(), 10, 10);
+			glPopMatrix();
+
+			util::rect<double> boundingBox(
+					sphere.getX() - sphere.getRadius(),
+					sphere.getY() - sphere.getRadius(),
+					sphere.getX() + sphere.getRadius(),
+					sphere.getY() + sphere.getRadius());
+
+			if (_size.area() == 0)
+				_size = boundingBox;
+			else
+				_size.fit(boundingBox);
+		}
+
+		gluDeleteQuadric(quadric);
+	}
 
 	stopRecording();
 }
